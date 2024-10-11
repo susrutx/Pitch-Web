cec9e1090c6fa460df1b0f5a350a25ce0bd44fad
pranjal_1604e <pranjal.dalu@gmail.com>
2024-10-11 15:12:48 +0530

    Footer changes

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -2,10 +2,10 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/extensions */
 
-import React from 'react';
-
-import BrandIcon from './BrandIcon';
-import Button from '../elements/Button';
+import React from "react";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import { Link } from "react-router-dom";
+import BrandIcon from "./BrandIcon";
 
 export default function Footer() {
   return (
@@ -15,50 +15,56 @@ export default function Footer() {
           <div className="w-1/3 flex-col ml-16 mr-8">
             <BrandIcon />
             <p className="w-full text-lg text-gray-400 font-light">
-              Growing Your Business
-              {' '}
+              Preserving Freshness
               <br />
-              Is Our Calling
+              with Precision and Care.
             </p>
           </div>
           <div className="w-1/3 mt-0 ml-16 mr-0 sm:ml-0 sm:mr-5">
-            <h1 className="text-lg text-theme-blue pt-4 pb-2">
-              Office
-            </h1>
+            <h1 className="text-lg text-theme-blue pt-4 pb-2">Office</h1>
             <p className="text-lg text-gray-400 font-light">
-              rac.hafidz@gmail.com
+              info@shreecoldstore.com
             </p>
-            <p className="text-lg text-gray-400 font-light">
-              Klaten, Central Java, Indonesia
+            <p className="text-md text-gray-400 font-light">
+              M/S Shree Cold Storage <br />
+              Village- Basilla, Nagri Panchayat- Devri Thana- Nagri (Ranchi)
+              <br />
+              Pin- 835303
             </p>
           </div>
           <div className="w-1/3 ml-16 sm:ml-0 mt-0">
-            <h1 className="text-lg text-theme-blue pt-4 pb-2">
-              Social
-            </h1>
-            <Button href="https://www.instagram.com/racmathafidz/" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
-              Instagram
-            </Button>
-            <Button href="https://www.linkedin.com/in/racmat-hafidz-89982a156/" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
-              LinkedIn
-            </Button>
-            <Button href="https://github.com/racmathafidz" type="link" target="_blank" className="flex text-lg text-gray-400 font-light hover:underline" isExternal>
-              Github
-            </Button>
+            <h1 className="text-lg text-theme-blue pt-4 pb-2">Social</h1>
+            <Link
+              to="/"
+              className="flex text-lg text-gray-400 font-light hover:underline"
+            >
+              Home
+            </Link>
+            <Link
+              to="/project"
+              className="flex text-lg text-gray-400 font-light hover:underline"
+            >
+              Project
+            </Link>
+            <Link
+              to="/team"
+              className="flex text-lg text-gray-400 font-light hover:underline"
+            >
+              Team
+            </Link>
+            <Link
+              to="/discuss-project"
+              className="flex text-lg text-gray-400 font-light hover:underline"
+            >
+              Discuss a Project
+            </Link>
           </div>
         </div>
         <div className="flex-col text-center mt-7">
-          <p className="text-lg text-gray-400 font-light">
-            Copyright 2021 - All rights reserved - Racxstudio
+          <p className="text-sm text-gray-600 font-light">
+            Copyright <CopyrightIcon fontSize="small" /> 2024 - All rights
+            reserved - Shree Cold Store
           </p>
-          <div className="flex-row">
-            <p className="inline-block text-lg text-gray-400 font-light">
-              Made with &#x2764; by&nbsp;
-            </p>
-            <Button href="https://github.com/racmathafidz" type="link" target="_blank" className="text-lg text-theme-purple font-light" isExternal>
-              Racmat Hafidz Fadli
-            </Button>
-          </div>
         </div>
       </div>
     </div>
